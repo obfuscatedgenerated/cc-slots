f74cc89e69b3e084df7e02d86527e022f96b1053
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-04-22 21:33:25 +0100

    split into files

---
+++ reels.lua
@@ -0,0 +1,26 @@
+local symbols = require "symbols"
+symbols = symbols.symbols
+
+-- define reel strip to use for all 3 reels
+local reel = {
+    symbols.cherry.name, symbols.cherry.name, symbols.cherry.name, symbols.cherry.name, symbols.cherry.name,
+    symbols.lemon.name, symbols.lemon.name, symbols.lemon.name, symbols.lemon.name,
+    symbols.orange.name, symbols.orange.name, symbols.orange.name, symbols.orange.name,
+    symbols.plum.name, symbols.plum.name, symbols.plum.name, symbols.plum.name,
+    symbols.grape.name, symbols.grape.name,
+    symbols.bar_single.name, symbols.bar_single.name, symbols.bar_single.name, symbols.bar_single.name,
+    symbols.bar_double.name, symbols.bar_double.name, symbols.bar_double.name,
+    symbols.bar_triple.name, symbols.bar_triple.name,
+    symbols.seven.name,
+    symbols.jackpot.name, symbols.jackpot.name,
+    symbols.bell.name,
+}
+
+local function random_reels()
+    return { math.random(1, #reel), math.random(1, #reel), math.random(1, #reel) }
+end
+
+return {
+    reel = reel,
+    random_reels = random_reels,
+}
---
+++ symbols.lua
@@ -0,0 +1,57 @@
+local obsi = require "/lib/obsi2"
+
+local symbols = {
+    cherry = { name = "cherry", image = nil, payout = { double = 1, triple = 2 } },
+    lemon = { name = "lemon", image = nil, payout = { triple = 5 } },
+    orange = { name = "orange", image = nil, payout = { triple = 4 } },
+    plum = { name = "plum", image = nil, payout = { triple = 6 } },
+    grape = { name = "grape", image = nil, payout = { triple = 3 } },
+    seven = { name = "seven", image = nil, payout = { triple = 50 } },
+    bell = { name = "bell", image = nil, payout = { triple = 8 } },
+    bar_single = { name = "bar_single", image = nil, payout = { triple = 10 } },
+    bar_double = { name = "bar_double", image = nil, payout = { triple = 20 } },
+    bar_triple = { name = "bar_triple", image = nil, payout = { triple = 30 } },
+    jackpot = { name = "jackpot", image = nil, payout = { triple = 100 } },
+}
+
+local TARGET_SIZE_X, TARGET_SIZE_Y = 24, 24
+
+local function load_symbol_images()
+    for k, v in pairs(symbols) do
+        v.image = obsi.graphics.newImage("symbols/" .. k .. ".nfp")
+
+        -- resize the image to the target size
+        if v.image.width < TARGET_SIZE_X or v.image.height < TARGET_SIZE_Y then
+            -- padding
+
+            local new_image = obsi.graphics.newBlankImage(TARGET_SIZE_X, TARGET_SIZE_Y, -1)
+
+            -- Center the old data in the new data
+            local x_offset = math.floor((TARGET_SIZE_X - v.image.width) / 2)
+            local y_offset = math.floor((TARGET_SIZE_Y - v.image.height) / 2)
+
+            for x = 1, v.image.width do
+                for y = 1, v.image.height do
+                    new_image.data[y + y_offset][x + x_offset] = v.image.data[y][x]
+                end
+            end
+            v.image = new_image
+        elseif v.image.width > TARGET_SIZE_X or v.image.height > TARGET_SIZE_Y then
+            -- crop the data and sizes to the target size
+
+            local new_image = obsi.graphics.newBlankImage(TARGET_SIZE_X, TARGET_SIZE_Y, -1)
+            for x = 1, TARGET_SIZE_X do
+                for y = 1, TARGET_SIZE_Y do
+                    new_image.data[y][x] = v.image.data[y][x]
+                end
+            end
+
+            v.image = new_image
+        end
+    end
+end
+
+return {
+    symbols = symbols,
+    load_symbol_images = load_symbol_images,
+}
