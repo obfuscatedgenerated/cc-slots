9fa88289c4f25fb66d6c652927c4dfc1a2abd2cc
Ollie <75035364+obfuscatedgenerated@users.noreply.github.com>
2025-04-25 14:29:24 +0100

    more fruit

--- reels.lua
+++ reels.lua
@@ -5,18 +5,20 @@ local SEED = 314
 
 -- define reel strip to use for all 3 reels
 local reel = {
-    symbols.cherry.name, symbols.cherry.name, symbols.cherry.name, symbols.cherry.name, symbols.cherry.name, symbols.cherry.name,
-    symbols.lemon.name, symbols.lemon.name, symbols.lemon.name, symbols.lemon.name, symbols.lemon.name,
-    symbols.orange.name, symbols.orange.name, symbols.orange.name, symbols.orange.name,
-    symbols.plum.name, symbols.plum.name, symbols.plum.name, symbols.plum.name,
-    symbols.grape.name, symbols.grape.name, symbols.grape.name,
+    symbols.cherry.name, symbols.cherry.name, symbols.cherry.name, symbols.cherry.name, symbols.cherry.name, symbols.cherry.name, symbols.cherry.name,
+    symbols.lemon.name, symbols.lemon.name, symbols.lemon.name, symbols.lemon.name, symbols.lemon.name, symbols.lemon.name,
+    symbols.orange.name, symbols.orange.name, symbols.orange.name, symbols.orange.name, symbols.orange.name,
+    symbols.plum.name, symbols.plum.name, symbols.plum.name, symbols.plum.name, symbols.plum.name,
+    symbols.grape.name, symbols.grape.name, symbols.grape.name, symbols.grape.name,
     symbols.bar_single.name, symbols.bar_single.name, symbols.bar_single.name, symbols.bar_single.name,
-    symbols.bar_double.name, symbols.bar_double.name, symbols.bar_double.name,
-    symbols.bar_triple.name, symbols.bar_triple.name,
+    symbols.bar_double.name, symbols.bar_double.name,
+    symbols.bar_triple.name,
     symbols.seven.name, symbols.seven.name,
     symbols.bell.name, symbols.bell.name,
     symbols.jackpot.name
 }
+
+-- TODO: add mixed bar, mixed fruit, etc payouts. the RTP is very bad right now!
 
 -- since we step through the reel each tick for the randomisation, we want to split apart the fruits being together (but still write the table in a readable way
 -- so shuffle it at require time with a fixed seed for consistency between plays
